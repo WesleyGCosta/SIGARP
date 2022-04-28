c9097abb8b2b711011c58e2df3177371a486a96a
Wesley Gomes <wesleygomes_costa@outlook.com>
2022-04-28 13:31:33 -0400

    Definindo Propriedades de ItemViewmodel

---
+++ src/WebApp/Controllers/ItemController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class ItemController : Controller
+    {
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/WebApp/Models/ItemViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApp.Models
+{
+    public class ItemViewModel
+    {
+        public int CodigoItem { get; set; }
+        public int CodigoAta { get; set; }
+        public int Ano { get; set; }
+        public int CodigoMateria { get; set; }
+        public int CodigoMarca { get; set; }
+        public int CodigounidadeAquisicao { get; set; }
+        public int ConsumoEstimado { get; set; }
+        public bool Ativo { get; set; }
+        public int Aditivo { get; set; }
+    }
+}

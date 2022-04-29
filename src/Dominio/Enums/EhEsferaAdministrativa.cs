bab2d6767365026a8846f1b16d13d3c22c3c23c7
Wesley Gomes <wesleygomes_costa@outlook.com>
2022-04-29 12:55:05 -0400

    Formulário de unidade administrativa

---
+++ src/Dominio/Enums/EhEsferaAdministrativa.cs
@@ -0,0 +1,9 @@
+﻿namespace Dominio.Enums
+{
+    public enum EhEsferaAdministrativa
+    {
+        Municipal = 1,
+        Estadual = 2,
+        Federal = 3
+    }
+}
---
+++ src/WebApp/Models/UnidadeAdministrativaViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Dominio.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models
+{
+    public class UnidadeAdministrativaViewModel
+    {
+        public int Id { get; set; }
+        [Display(Name = "Unidade ou Órgão")]
+        public string NomeUnidadeAdministrativa { get; set; }
+        [Display(Name = "Sigla")]
+        public string Sigla { get; set; }
+        [Display(Name = "Admin. Direta / Indireta do Governo RO?")]
+        public bool OrgaoEx { get; set; }
+        [Display(Name = "Unidade da Federação")]
+        public string UnidadeDaFederacao { get; set; }
+        [Display(Name = "Esfera Administrativa")]
+        public EhEsferaAdministrativa EsferaAdministrativa { get; set; }
+        public bool Ativo { get; set; }
+
+    }
+}

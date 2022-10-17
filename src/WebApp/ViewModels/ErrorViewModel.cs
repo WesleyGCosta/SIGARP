5b8cc39222855097361c83851533a74a285fcb41
Wesley Gomes <wesleygomes_costa@outlook.com>
2022-10-17 11:29:42 -0400

    Adicionando páginas de erros 403, 404 e 500

--- src/WebApp/ViewModels/ErrorViewModel.cs
+++ src/WebApp/ViewModels/ErrorViewModel.cs
@@ -2,8 +2,8 @@ namespace WebApp.ViewModels
 {
     public class ErrorViewModel
     {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public int ErroCode { get; set; }
+        public string Title { get; set; }
+        public string Message { get; set; }
     }
 }

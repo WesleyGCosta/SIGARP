3a53875a27835cd4273c2ecd7524b54c1aa7f2cc
Wesley Gomes <wesleygomes_costa@outlook.com>
2022-11-29 12:08:01 -0400

    Baixando pacote rotativa e configurando para gerar PDF

--- src/WebApp/Controllers/ReportController.cs
+++ src/WebApp/Controllers/ReportController.cs
@@ -3,8 +3,12 @@ using Domain.Notifications.Interface;
 using Historia.Atas;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Rotativa.AspNetCore;
+using Rotativa.AspNetCore.Options;
+using System;
 using System.Threading.Tasks;
 using WebApp.Factories;
+using WebApp.ViewModels;
 
 namespace WebApp.Controllers
 {
@@ -25,11 +29,35 @@ namespace WebApp.Controllers
 
         public async Task<IActionResult> GetAtaReport(int yearAta, int codeAta)
         {
+            return PartialView("_ReportAtaDetails", await GetAtaMap(yearAta, codeAta));
+        }
+
+        public async Task<IActionResult> GeneratePdf(int yearAta, int codeAta)
+        {
+            var ataViewModel = await GetAtaMap(yearAta, codeAta);
+            var customSwitches = "--header-spacing 15 --footer-font-size 10 --footer-center  \" Emitido em : " + DateTime.Now + " - SIGARP \n  Sistema de Gerenciamento de Atas de Registro de Preço  [page]-[toPage] \"  --footer-spacing 8 ";
+            var view = new ViewAsPdf
+            {
+                Model = ataViewModel,
+                ViewName = "DespachoFinal",
+                PageSize = Size.A4,
+                IsGrayScale = false,
+                PageOrientation = Orientation.Portrait,
+                PageMargins = new Margins(10, 1, 20, 1),
+                CustomSwitches = customSwitches,
+                PageHeight = 630,
+                PageWidth = 300
+            };
+
+            return view;
+        }
+
+        private async Task<AtaViewModel> GetAtaMap(int yearAta, int codeAta)
+        {
             var ata = await _searchAta.GetAtaPublish(yearAta, codeAta, true);
 
             var ataViewModel = AtaFactory.ToViewModel(ata);
-
-            return PartialView("_ReportAtaDetails", ataViewModel);
+            return ataViewModel;
         }
     }
 }
